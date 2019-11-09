4bf1ebe18bdb050168b6ca2bc05e780c707b2044
DentonWarnock <dentonwarnock@gmail.com>
2019-11-08 19:35:32 -0800

    PlayersList and PlayerCard components updated and styled.

--- client/src/components/PlayersList.js
+++ client/src/components/PlayersList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PlayerCard from './PlayerCard';
 
 export default function PlayersList(props) {
 
     return (
-        <div>
-            <h3>Players Ranked by Search Interest</h3>
+        <div className="players-container">
+            <h2>Players Ranked by Search Interest</h2>
+            <div className="players-list">            
+                {props.playersList.map(player => {
+                return <PlayerCard player={player} />
+                })}
+            </div>
         </div>
     )
 }
